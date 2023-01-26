7bba3f60c57007828c4836c0e32e0f91752b6c8c
LAPTOP-J69OJ06J\Simone <simonedamato8@gmail.com>
2023-01-26 16:23:05 +0100

    Progetto nuovo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'}, 
+  {path: 'home', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
