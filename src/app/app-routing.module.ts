28a7ed6228e4243dcc27d7b32477c6a10fbfbe0c
Alessandro <alexrotondo58@gmail.com>
2023-01-26 18:02:16 +0100

    sistemazione app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'}, 
-  {path: 'home', component: MenuComponent}
+const routes: Routes = [ 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: MenuComponent,
+    children: [
+      { path: 'home', component: HomeComponent }
+    ]
+  }
 ];
 
 @NgModule({
