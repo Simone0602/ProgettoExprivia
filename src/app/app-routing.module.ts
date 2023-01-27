5713ee4065ef06f491fc2a6ab03bbfa0ff720ee2
LAPTOP-J69OJ06J\Simone <simonedamato8@gmail.com>
2023-01-27 15:41:09 +0100

    Niente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
-import { LoginStudenteComponent } from './login-studente/login-studente.component';
 
 const routes: Routes = [ 
   { path: '', component: MenuComponent,
@@ -10,8 +9,7 @@ const routes: Routes = [
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent }
     ]
-  },
-  {path: 'login-studente', component: LoginStudenteComponent}
+  }
 ];
 
 @NgModule({
