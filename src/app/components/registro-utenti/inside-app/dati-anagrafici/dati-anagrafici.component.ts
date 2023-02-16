cf94a2a35d7043feb89eff34cf77c1eddd7e87a6
Alessandro <alexrotondo58@gmail.com>
2023-02-16 11:07:06 +0100

    sistemazione form group

--- src/app/components/registro-utenti/inside-app/dati-anagrafici/dati-anagrafici.component.ts
+++ src/app/components/registro-utenti/inside-app/dati-anagrafici/dati-anagrafici.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Docente } from 'src/app/class/Docente';
 import { Studente } from 'src/app/class/Studente';
@@ -9,21 +10,46 @@ import { LoginService } from 'src/app/service/login-service/service-dati/login.s
   templateUrl: './dati-anagrafici.component.html',
   styleUrls: ['./dati-anagrafici.component.css']
 })
-export class DatiAnagraficiComponent implements OnInit{
-  
+export class DatiAnagraficiComponent implements OnInit {
+
+  routingUser_studenteOrDocente: boolean;
+
+  formDocente: FormGroup;
+  formStudente: FormGroup;
+
   private user: Studente | Docente;
 
   constructor(private loginService: LoginService,
-    private router: Router){}
+    private router: Router) { }
 
-  ngOnInit(): void { 
-    if(this.router.url.includes('registro-docente')){
+  ngOnInit(): void {
+    this.formStudente = this.createFormGroup()
+    this.formDocente = this.createFormGroup()
+    this.gestioneRoutin();
+  }
+
+  private createFormGroup(): FormGroup{
+    const formUser = new FormGroup({
+      nome: new FormControl(null, [Validators.required]),
+      cognome: new FormControl(null, [Validators.required]),
+      mail: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+    });
+    return formUser;
+  }
+  private gestioneRoutin(): void{
+    if (this.router.url.includes('registro-docente')) {
       this.user = this.loginService.getDocente();
-    }else{
+      this.routingUser_studenteOrDocente = true;
+      this.formDocente.addControl('codiceFiscale', new FormControl(null, [Validators.required, Validators.minLength(16), Validators.maxLength(16)]))
+    } else {
       this.user = this.loginService.getStudente();
+      this.routingUser_studenteOrDocente = false;
     }
-    console.log(this.user);
-    
+  }
+
+  getUser(): Studente | Docente {
+    return this.user;
   }
 
 }
